6a10a5985d8757b955e3176202d946a7060deb96
A00837418 <a00837418@tec.mx>
2025-05-21 00:36:08 -0600

    1

--- main.cpp
+++ main.cpp
@@ -1,113 +1,9 @@
-/*
- *  E1. Actividad Integradora 1
- * 
- *  Descripción: Programa que lee archivos de texto y código malicioso, detecta patrones,
- *              encuentra palíndromos y substrings comunes.
- *  Programadores : 
- *   
- *   Francisco Eduardo Pérez Berlanga - A00837418
- *
- *   Paulo de Jesus Ibarra Carballo a00837745 
- *
- *   Claudio Patricio Cantú Alanís a00835868
- *  
- *    Pedro Alejandro Rodriguez Gonzalez a00837473
- *
- *   Diego José Roca Rodríguez a00836390
- *
- *  Fecha: 5 de abril de 2025
- */
-
-
 #include <iostream>
-#include <fstream>
 #include <string>
 #include <algorithm>
-#include <vector>
-#include <utility>
+#include "text_analysis.h"
 
 using namespace std;
-
-string leerArchivo(const string& ruta) {
-    ifstream archivo(ruta);
-    if (!archivo.is_open()) {
-        cerr << "No se pudo abrir el archivo: " << ruta << endl;
-        return "";
-    }
-    string contenido((istreambuf_iterator<char>(archivo)), istreambuf_iterator<char>());
-    contenido.erase(remove(contenido.begin(), contenido.end(), '\r'), contenido.end());
-    return contenido;
-}
-
-pair<bool, size_t> buscarPatronConPosicion(const string& texto, const string& patron) {
-    size_t pos = texto.find(patron);
-    if (pos == string::npos) return {false, 0};
-    return {true, pos + 1}; // 1-based
-}
-
-pair<pair<size_t, size_t>, string> encontrarPalindromoReal(const string& s) {
-    size_t n = s.length();
-    if (n == 0) return {{0, 0}, ""};
-
-    size_t max_len = 1;
-    size_t start = 0;
-
-    for (size_t i = 0; i < n; i++) {
-        // Palíndromos impares
-        size_t l = i, r = i;
-        while (l <= r && r < n && s[l] == s[r]) {
-            if (r - l + 1 > max_len) {
-                max_len = r - l + 1;
-                start = l;
-            }
-            if (l == 0) break;
-            l--;
-            r++;
-        }
-
-        // Palíndromos pares
-        l = i;
-        r = i + 1;
-        while (l <= r && r < n && s[l] == s[r]) {
-            if (r - l + 1 > max_len) {
-                max_len = r - l + 1;
-                start = l;
-            }
-            if (l == 0) break;
-            l--;
-            r++;
-        }
-    }
-
-    string palindromo = s.substr(start, max_len);
-    size_t pos_final = start + max_len;
-    return {{start + 1, pos_final}, palindromo};
-}
-
-pair<pair<size_t, size_t>, string> encontrarSubstringComunReal(const string& s1, const string& s2) {
-    size_t m = s1.length();
-    size_t n = s2.length();
-    vector<vector<size_t>> dp(m + 1, vector<size_t>(n + 1, 0));
-    size_t max_len = 0;
-    size_t end = 0;
-
-    for (size_t i = 1; i <= m; i++) {
-        for (size_t j = 1; j <= n; j++) {
-            if (s1[i-1] == s2[j-1]) {
-                dp[i][j] = dp[i-1][j-1] + 1;
-                if (dp[i][j] > max_len) {
-                    max_len = dp[i][j];
-                    end = i;
-                }
-            }
-        }
-    }
-
-    if (max_len == 0) return {{0, 0}, ""};
-
-    string substring = s1.substr(end - max_len, max_len);
-    return {{end - max_len + 1, end}, substring};
-}
 
 int main() {
     string t1 = leerArchivo("Textos/transmission1.txt");
@@ -117,7 +13,7 @@ int main() {
     string m3 = leerArchivo("Textos/mcode3.txt");
 
     cout << "Parte 1:" << endl;
-    
+
     auto [found1, pos1] = buscarPatronConPosicion(t1, m1);
     cout << (found1 ? "true " + to_string(pos1) : "false") << endl;
 
@@ -143,7 +39,7 @@ int main() {
 
     auto [pal2_pos, pal2_str] = encontrarPalindromoReal(t2);
     replace(pal2_str.begin(), pal2_str.end(), '\n', ' ');
-    cout << pal2_pos.first << " " << pal2_pos.second << "" << pal2_str << endl;
+    cout << pal2_pos.first << " " << pal2_pos.second << " " << pal2_str << endl;
 
     cout << "Parte 3:" << endl;
     auto [sub_pos, sub_str] = encontrarSubstringComunReal(t1, t2);
---
+++ text_analysis.h
@@ -0,0 +1,17 @@
+#ifndef TEXT_ANALYSIS_H
+#define TEXT_ANALYSIS_H
+
+#include <string>
+#include <utility>
+
+using namespace std;
+
+string leerArchivo(const string& ruta);
+
+pair<bool, size_t> buscarPatronConPosicion(const string& texto, const string& patron);
+
+pair<pair<size_t, size_t>, string> encontrarPalindromoReal(const string& s);
+
+pair<pair<size_t, size_t>, string> encontrarSubstringComunReal(const string& s1, const string& s2);
+
+#endif
