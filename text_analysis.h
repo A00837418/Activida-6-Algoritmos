3f801964e8f69bd04e505df6a6d22174241f48d7
A00837418 <a00837418@tec.mx>
2025-05-21 01:17:06 -0600

    1

--- text_analysis.h
+++ text_analysis.h
@@ -1,4 +1,5 @@
 #ifndef TEXT_ANALYSIS_H
+
 #define TEXT_ANALYSIS_H
 
 #include <string>
@@ -6,16 +7,12 @@
 
 using namespace std;
 
-// Lee todo el contenido de un archivo dado su ruta
 string leerArchivo(const string& ruta);
 
-// Busca un patrón en un texto; devuelve si se encontró y la posición
 pair<bool, size_t> buscarPatronConPosicion(const string& texto, const string& patron);
 
-// Encuentra el palíndromo más largo en una cadena; devuelve posición y subcadena
 pair<pair<size_t, size_t>, string> encontrarPalindromoReal(const string& s);
 
-// Encuentra la subcadena común más larga entre dos cadenas; devuelve posición y subcadena
 pair<pair<size_t, size_t>, string> encontrarSubstringComunReal(const string& s1, const string& s2);
 
 #endif
