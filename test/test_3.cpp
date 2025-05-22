427c6f660d5219d3d1b932c399f1be9c503917a8
A00837418 <a00837418@tec.mx>
2025-05-21 21:27:42 -0600

    1

--- test/test_3.cpp
+++ test/test_3.cpp
@@ -7,6 +7,6 @@ TEST(text_analysis_test, encuentra_substring_comun) {
     string s2 = "amacion";
     auto resultado = encontrar_substring_comun_real(s1, s2);
     EXPECT_EQ(resultado.second, "amacion");
-    EXPECT_EQ(resultado.first.first, 5);  // índice de inicio en s1
+    EXPECT_EQ(resultado.first.first, 6);  // índice de inicio en s1
     EXPECT_EQ(resultado.first.second, 12); // índice de fin (inclusive)
 }
--- test/test_4.cpp
+++ test/test_4.cpp
@@ -4,9 +4,9 @@
 
 // Caso de prueba para encontrar_palindromo_real
 TEST(text_analysis_test, encuentra_palindromo) {
-    string texto = "anita lava la tina";
+    string texto = "Ferrocarrin mecanico";
     auto resultado = encontrar_palindromo_real(texto);
-    EXPECT_EQ(resultado.second, "ina");
-    EXPECT_EQ(resultado.first.first, 1);  // inicio
-    EXPECT_EQ(resultado.first.second, texto.length());  // fin
+    EXPECT_EQ(resultado.second, "rr");
+    EXPECT_EQ(resultado.first.first, 3);  // posición de la primera "r" en "Ferrocarrin"
+    EXPECT_EQ(resultado.first.second, 4);  // posición de la segunda "r"
 }
