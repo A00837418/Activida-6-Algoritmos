5f135dd4878869e77b81d39010aa4d009d164541
A00837418 <a00837418@tec.mx>
2025-05-21 21:09:24 -0600

    1

---
+++ test/test_1.cpp
@@ -0,0 +1,11 @@
+#include <gtest/gtest.h>
+#include "../src/text_analysis.h"  // Ajusta la ruta si está en otra carpeta
+
+// Caso de prueba para buscar_patron_con_posicion
+TEST(text_analysis_test, busca_patron) {
+    string texto = "Hola mundo desde C++";
+    string patron = "mundo";
+    auto resultado = buscar_patron_con_posicion(texto, patron);
+    EXPECT_TRUE(resultado.first);
+    EXPECT_EQ(resultado.second, 6);  // "mundo" comienza en posición 6 (1-based)
+}
