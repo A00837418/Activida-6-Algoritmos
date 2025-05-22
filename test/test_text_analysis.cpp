57cefb4f633138f568740f19e9497d87ddbeb1c2
A00837418 <a00837418@tec.mx>
2025-05-21 20:33:24 -0600

    1

--- test/test_text_analysis.cpp
+++ test/test_text_analysis.cpp
@@ -1,29 +1,8 @@
 
-/*
- *  E1. Actividad Integradora 1
- * 
- *  Descripción: Programa que lee archivos de texto y código malicioso, detecta patrones,
- *              encuentra palíndromos y substrings comunes.
- *  Programadores : 
- *   
- *   Francisco Eduardo Pérez Berlanga - A00837418
- *
- *   Paulo de Jesus Ibarra Carballo a00837745 
- *
- *   Claudio Patricio Cantú Alanís a00835868
- *  
- *    Pedro Alejandro Rodriguez Gonzalez a00837473
- *
- *   Diego José Roca Rodríguez a00836390
- *
- *  Fecha: 5 de abril de 2025
- */
-
 #include "text_analysis.h"
 #include <gtest/gtest.h>
 
-
-TEST(TextAnalysisTest, EncuentraPalindromo) {
+TEST(text_analysis_test, encuentra_palindromo) {
     string texto = "anita lava la tina";
     auto [pos, pal] = encontrar_palindromo_real(texto);
     EXPECT_EQ(pal, "anita lava la tina");
