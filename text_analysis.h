4b481ff3c0461503041a36c09ecb19af71bfc463
A00837418 <a00837418@tec.mx>
2025-05-21 19:28:36 -0600

    1

--- text_analysis.h
+++ text_analysis.h
@@ -1,3 +1,24 @@
+
+/*
+ * SonarQube, herramienta de gestión de calidad de software de código abierto.
+ * Copyright (C) 2008-2013 SonarSource
+ * http://github.com/SonarOpenCommunity/sonar-cxx
+ *
+ * SonarQube es un software libre; puedes redistribuirlo y/o
+ * modificarlo según los términos de la Licencia Pública General Reducida de GNU
+ * Licencia publicada por la Free Software Foundation; ya sea
+ * versión 3 de la Licencia, o (a su elección) cualquier versión posterior.
+ *
+ * SonarQube se distribuye con la esperanza de que sea útil,
+ * pero SIN NINGUNA GARANTÍA; sin siquiera la garantía implícita de
+ * COMERCIABILIDAD o IDONEIDAD PARA UN PROPÓSITO PARTICULAR. Véase la GNU
+ * Licencia Pública General Reducida para más detalles.
+ *
+ * Debería haber recibido una copia de la Licencia Pública General Reducida de GNU
+ * junto con este programa; si no, escriba a la Free Software Foundation,
+ * Inc., 51 Franklin Street, quinto piso, Boston, MA 02110-1301, EE. UU.
+ */
+
 #ifndef TEXT_ANALYSIS_H
 #define TEXT_ANALYSIS_H
 
@@ -56,7 +77,9 @@ void expandir_desde_centro(const string& s, size_t left, size_t right, size_t& m
             max_len = longitud_actual;
             start = left;
         }
-        if (left == 0) break;
+        if (left == 0) {
+            break;
+        }
         left--;
         right++;
     }
