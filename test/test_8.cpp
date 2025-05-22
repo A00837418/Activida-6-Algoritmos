7a0e511d6f0da87e7dc41f4e5a8314f82c12145a
A00837418 <a00837418@tec.mx>
2025-05-21 22:24:24 -0600

    1

--- test/test_8.cpp
+++ test/test_8.cpp
@@ -1,11 +1,16 @@
 #include <gtest/gtest.h>
 #include "text_analysis.h"
 
-pair<bool, size_t> buscar_patron_con_posicion(const string& texto, const string& patron) {
-    size_t pos = texto.find(patron);
-    if (pos == string::npos) {
-Uncovered code
-        return {false, 0};
-    }
-    return {true, pos + 1};
+// Test para encontrar_substring_comun_real
+TEST(TextAnalysisTest, SubstringComun) {
+    auto resultado = encontrar_substring_comun_real("computadora", "toradora");
+    EXPECT_EQ(resultado.second, "adora");
+    EXPECT_EQ(resultado.first.second - resultado.first.first + 1, resultado.second.length());
+}
+
+TEST(TextAnalysisTest, SinSubstringComun) {
+    auto resultado = encontrar_substring_comun_real("abc", "xyz");
+    EXPECT_EQ(resultado.second, "");
+    EXPECT_EQ(resultado.first.first, 0);
+    EXPECT_EQ(resultado.first.second, 0);
 }
