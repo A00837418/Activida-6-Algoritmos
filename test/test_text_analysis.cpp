c86cf73990ed1897019aec6f5ce0fa5df1e017c0
A00837418 <a00837418@tec.mx>
2025-05-21 20:36:30 -0600

    1

--- test/test_text_analysis.cpp
+++ test/test_text_analysis.cpp
@@ -5,5 +5,5 @@
 TEST(text_analysis_test, encuentra_palindromo) {
     string texto = "anita lava la tina";
     auto [pos, pal] = encontrar_palindromo_real(texto);
-    EXPECT_EQ(pal, "anita lava la tina");
+    EXPECT_EQ(pal, "ava");
 }
