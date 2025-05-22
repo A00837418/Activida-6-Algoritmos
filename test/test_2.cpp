657e1654650dc4fec7bb52fcff884894d3088d5c
A00837418 <a00837418@tec.mx>
2025-05-21 23:00:42 -0600

    1

--- test/test_2.cpp
+++ test/test_2.cpp
@@ -2,12 +2,6 @@
 #include "text_analysis.h"
 #include <gtest/gtest.h>
 
-TEST(TextAnalysisTest, encuentra_palindromo) {
-    string texto = "anita lava la tina";
-    auto [pos, pal] = encontrar_palindromo_real(texto);
-    EXPECT_EQ(pal, "ava");
-}
-
 // Test para encontrar_palindromo_real
 TEST(TextAnalysisTest, PalindromoSimple) {
     auto resultado = encontrar_palindromo_real("abcba");
@@ -24,22 +18,4 @@ TEST(TextAnalysisTest, PalindromoPar) {
 TEST(TextAnalysisTest, PalindromoEnMedio) {
     auto resultado = encontrar_palindromo_real("xyzracecar123");
     EXPECT_EQ(resultado.second, "racecar");
-}
-
-TEST(TextAnalysisTest, PalindromoCadenaVacia) {
-    std::string vacio = "";
-    auto resultado = encontrar_palindromo_real(vacio);
-
-    EXPECT_EQ(resultado.first.first, 0);
-    EXPECT_EQ(resultado.first.second, 0);
-    EXPECT_EQ(resultado.second, "");
-}
-
-// Caso de prueba para encontrar_palindromo_real
-TEST(TextAnalysisTest, encuentra_palindromo) {
-    string texto = "Ferrocarrin mecanico";
-    auto resultado = encontrar_palindromo_real(texto);
-    EXPECT_EQ(resultado.second, "rr");
-    EXPECT_EQ(resultado.first.first, 3);  // posición de la primera "r" en "Ferrocarrin"
-    EXPECT_EQ(resultado.first.second, 4);  // posición de la segunda "r"
 }
