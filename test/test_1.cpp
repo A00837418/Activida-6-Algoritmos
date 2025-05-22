a669c38ed95ae4d47459bb1910fc61fb48bf8652
A00837418 <a00837418@tec.mx>
2025-05-22 08:44:32 -0600

    1

--- test/test_1.cpp
+++ test/test_1.cpp
@@ -2,10 +2,11 @@
 #include <gtest/gtest.h>
 #include "text_analysis.h"  // Ajusta la ruta si está en otra carpeta
 
-TEST(BuscarPatronTest, PatronPresente) {
-    std::string texto = "abcde123abcde";
-    std::string patron = "123";
-    auto resultado = buscar_patron_con_posicion(texto, patron);
-    EXPECT_TRUE(resultado.first);
-    EXPECT_EQ(resultado.second, 6);
+TEST(text_analysis_test, substring_comun_en_medio) {
+    string s1 = "ferrocarril";
+    string s2 = "bicirrin";
+    auto resultado = encontrar_substring_comun_real(s1, s2);
+    EXPECT_EQ(resultado.second, "rri");
+    EXPECT_EQ(resultado.first.first, 7);  // 'r' de "carril" en índice 7
+    EXPECT_EQ(resultado.first.second, 9); // termina en 'i' (índice 9)
 }
